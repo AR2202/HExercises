d856d72a1e226be2ea53683083ffa1567bbe7ffb
Annika Rings <annika.rings@dpag.ox.ac.uk>
2020-06-28 21:39:47 +0100

    added Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,36 @@
 module Main where
 
-import Lib
+
+import Chapter23
+
+data CoordCheck = Valid|NotValid deriving (Show,Read,Eq)
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "Enter Top Left Coordinate (x,y)"
+  coord1  <-fmap readCoord getLine
+  putStrLn "Enter Bottom Right Coordinate (x,y)"
+  coord2  <-fmap readCoord getLine
+  putStrLn "Please chose a start State (1-3)"
+  let coordValid = checkCoords coord1 coord2
+  case coordValid of
+    NotValid -> putStrLn "left coordinate must have smaller x and y values than right coordinate"
+    Valid -> do
+      option<-getLine
+      let selection = lookup option startoptions
+      case selection of
+        Nothing -> do
+          putStrLn "Not a valid selection"
+     
+        Just startState -> animateGrid coord1 coord2 startState
+
+
+
+
+checkCoords :: Coord -> Coord -> CoordCheck
+checkCoords (x1,y1) (x2,y2)
+  |x1<=x2 && y1<=y2 = Valid
+  |otherwise        = NotValid
+
+readCoord :: String -> Coord
+readCoord = read

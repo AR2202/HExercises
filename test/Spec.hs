bfba8a6367c423c3c8c66a4fbaf68623f2251db6
Annika Rings <annika.rings@dpag.ox.ac.uk>
2020-08-14 21:41:33 +0100

    changed test

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,37 +7,44 @@ main :: IO ()
 main = hspec $ 
   describe "fizzes" $ do
     it "should have fizz as every third element" 
-      $ property $
-        \(NonNegative x::NonNegative Int)-> fizzes !! (3*x +2) == Just "fizz"
+      $ property prop_fizzes_every_third
+      
     describe "buzzes" $ do
       it "should have buzz as every fifth element"
-        $ property $
-        \(NonNegative x::NonNegative Int)-> buzzes !! (5*x +4) == Just "buzz"
+        $ property prop_buzzes_every_fifth
+
       describe "fizzbuzzes" $ do
         it "should have fizzbuzz as every fifteenth element"
-          $property$
-          \(NonNegative x::NonNegative Int)-> fizzbuzzes !! (15*x +14) == Just "fizzbuzz"
-          
+          $property prop_fizzbuzz_every_fifteenth
+
         describe "numfizzbuzz" $ do
           it "should have Strings, not Maybe"
-            $property$
-            \(NonNegative x::NonNegative Int)-> numfizzbuzz !! (15*x +14) == "fizzbuzz"
-           
-          describe "numfizzbuzz" $ do
-            it "should have number if it is not divisible by 3 or 5"
-              $property$
-              \(NonNegative x::NonNegative Int)-> x `mod`5>0 && x`mod`3 >0==> numfizzbuzz !! (x-1) == show x
-
-            describe "neighbours" $ do
-              it "should not contain the element itself"
-                $property prop_neighbours_noSelf
+            $property prop_numfizzbuzz_string
+                    
+          it "should have number if it is not divisible by 3 or 5"
+            $property prop_numfizzbuzz_numb
              
-              it "should be symmetric"
-                $property prop_neighbours_symmetric
+          describe "neighbours" $ do
+            it "should not contain the element itself"
+              $property prop_neighbours_noSelf
+             
+            it "should be symmetric"
+              $property prop_neighbours_symmetric
 
-                
+prop_fizzes_every_third :: NonNegative Int -> Bool
+prop_fizzes_every_third (NonNegative x) = fizzes !! (3*x +2) == Just "fizz"
+
+prop_buzzes_every_fifth :: NonNegative Int -> Bool
+prop_buzzes_every_fifth (NonNegative x) = buzzes !! (5*x +4) == Just "buzz"
+
+prop_fizzbuzz_every_fifteenth :: NonNegative Int-> Bool
+prop_fizzbuzz_every_fifteenth (NonNegative x) = fizzbuzzes !! (15*x +14) == Just "fizzbuzz"
+
+prop_numfizzbuzz_string :: NonNegative Int -> Bool
+prop_numfizzbuzz_string (NonNegative x) = numfizzbuzz !! (15*x +14) == "fizzbuzz"       
             
-
+prop_numfizzbuzz_numb:: NonNegative Int -> Property
+prop_numfizzbuzz_numb (NonNegative x )= x `mod`5>0 && x`mod`3 >0==> numfizzbuzz !! (x-1) == show x
 
 prop_neighbours_noSelf :: Coord -> Bool
 prop_neighbours_noSelf coord = coord `notElem` (neighbours coord)
